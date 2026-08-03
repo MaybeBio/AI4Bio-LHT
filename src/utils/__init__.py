b9c0dad0c894f55e33b3f03f3d273ff1e3a48d4f
Joe Hoye Dow <luxunisgod123@gmail.com>
2026-08-03 21:20:37 +0800

    update src/utils/init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,22 @@
+# original refer to: https://github.com/ashleve/lightning-hydra-template/blob/main/src/utils/__init__.py
+# modify refer to: https://github.com/nathanpainchaud/lightning-hydra-template/blob/main/src/lightning_hydra_template/utils/__init__.py
+
 from src.utils.instantiators import instantiate_callbacks, instantiate_loggers
-from src.utils.logging_utils import log_hyperparameters
+from src.utils.logging_utils import log_hyperparameters, pad_keys
 from src.utils.pylogger import RankedLogger
 from src.utils.rich_utils import enforce_tags, print_config_tree
 from src.utils.utils import extras, get_metric_value, task_wrapper
+
+# just add all above to __all__ to make them available for import *
+__all__ = [
+    "RankedLogger",
+    "enforce_tags",
+    "extras",
+    "get_metric_value",
+    "instantiate_callbacks",
+    "instantiate_loggers",
+    "log_hyperparameters",
+    "pad_keys",
+    "print_config_tree",
+    "task_wrapper",
+]
